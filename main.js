c8e7236af7087467cab18082fb0842666658d121
EDUARDA DE JESUS SILVA <24122366@CFP127ALUNO.LOCAL>
2025-04-01 12:24:29 -0300

    inicio da div contatos

---
+++ main.js
@@ -0,0 +1,40 @@
+//serve para mostrar os erros ocultos no terminal
+"use strict"
+
+async function mostrarContatos (){
+    const url = `https://giovanna-whatsapp.onrender.com/v1/whatsapp/contatos/11987876567`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+} 
+
+function criarContatos (link){
+    const divContatos = document.getElementById('contatos')
+    const card = document.createElement('div')
+    card.classList.add('cards')
+
+
+    const novoNome =  document.createElement('h2')
+    novoNome.textContent = link.nome
+
+    const novaImg = document.createElement('img')
+    novaImg.src = link.foto
+
+    card.appendChild(novaImg)
+    card.appendChild(novoNome)
+
+    divContatos.appendChild(card)
+
+}
+
+async function preencherContatos(){
+  
+    const contatos = await mostrarContatos()
+   
+    contatos.contatos[0].forEach(criarContatos).
+
+    console.log (contatos) 
+}
+
+preencherContatos()
