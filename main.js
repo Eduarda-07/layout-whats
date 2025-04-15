30625c1e53629ff3da3786c0d162b29bbfbcac1a
EDUARDA DE JESUS SILVA <24122366@CFP127ALUNO.LOCAL>
2025-04-15 12:27:28 -0300

    conversas parte 1

--- main.js
+++ main.js
@@ -18,13 +18,15 @@ function criarContatos (link){
     const novoNome =  document.createElement('h2')
     novoNome.textContent = link.name
 
-    const novaImg = document.createElement('img')
-    novaImg.src = link.profile
-
-    card.appendChild(novaImg)
+ 
     card.appendChild(novoNome)
 
     divContatos.appendChild(card)
+
+    card.addEventListener('click',async function(){
+        await preencherConversas(link.name)
+    })
+
 
 }
 
@@ -35,6 +37,45 @@ async function preencherContatos(){
     contatos.dados_contato.forEach(criarContatos)
 
     console.log (contatos) 
+}
+
+
+
+async function mostrarConversas (name){
+    const url = `https://giovanna-whatsapp.onrender.com/v1/whatsapp/conversas?numero=11987876567&contato=${name}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+} 
+
+async function criarConversas (name){
+    const divConversas = document.getElementById('conversas')
+    const menssagem = document.createElement('div')
+    menssagem.classList.add('menssagem')
+
+    const novaMenssagem =  document.createElement('p')
+    const time1 =  document.createElement('small')
+
+    novaMenssagem.textContent = name.conversas.content
+    time1.textContent = name.conversas.time
+    // const novaResposta =  document.createElement('p')
+    // const time2 =  document.createElement('small')
+    menssagem.appendChild(novaMenssagem)
+    menssagem.appendChild(time1)
+
+    divConversas.appendChild(menssagem)
+
+}
+
+async function preencherConversas(name){
+  
+    const dados = await mostrarConversas(name)
+   
+    const conversa = dados.conversas
+    criarConversas(conversa)
+
+    console.log (conversa) 
 }
 
 preencherContatos()
